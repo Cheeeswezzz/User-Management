a3a89349b5599a0704f45e5be131efdaec7368db
Rafael D. Caballero <caballerorafael211@gmail.com>
2025-05-16 19:06:55 -0700

    Initial commit

--- frontend/src/app/_models/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './account';
-export * from './alert';
-export * from './role';
-export * from './employee';
-export * from './department';
-export * from './workflow';
-export * from './request';
